fdd9631e6e6cdf31aeef42cd635eeeee7518673d
Corey Burrows <corey.burrows@gmail.com>
2022-12-28 09:35:01 -0600

    Setup build.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Repo, {MapperAction} from './Repo';
+import Model, {Mapper, MapperError, NullMapper, RawRecord} from './Model';
+import Query from './Query';
+
+export {Model, Query, Mapper, MapperError, MapperAction, NullMapper, RawRecord};
+export default Repo;
